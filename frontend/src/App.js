ed54ebbd29d3019b2ce1a4c73ee16bc191bde92b
mohseneptune <mohseneptune@gmail.com>
2023-05-16 13:47:29 +0330

    add basic react to the project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Typography from '@mui/material/Typography'
+function App() {
+  return (
+    <>
+    <Typography variant="h4">
+      Hello World !!!
+    </Typography>
+    </>
+  );
+}
+
+export default App;
